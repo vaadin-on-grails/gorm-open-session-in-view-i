9e199c2ca184719e6ac1e061ebf06dc5fe7f66ba
Ondrej Kvasnovsky <okvasnovsky@vendavo.com>
2014-11-08 23:49:00 -0800

    Example of how to enable OSIV.

---
+++ src/groovy/app/MyUI.groovy
@@ -0,0 +1,33 @@
+package app
+
+import com.myapp.User
+import com.vaadin.data.util.BeanItem
+import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.ComboBox
+import com.vaadin.ui.UI
+import com.vaadin.ui.VerticalLayout
+
+/**
+ *
+ *
+ * @author
+ */
+class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        VerticalLayout layout = new VerticalLayout()
+        layout.setMargin(true)
+
+        ComboBox userAccount = new ComboBox()
+
+        User user = User.findAll()[0]
+        BeanItem item = new BeanItem(user, 'label')
+
+        userAccount.addItem(item)
+
+        layout.addComponent(userAccount)
+        setContent(layout)
+    }
+}
